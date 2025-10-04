ed7a80608db5c71d36363467617793b68e4f0c31
avagimyan7 <ashotavagimyan24@gmail.com>
2025-10-05 00:07:54 +0400

    feat: seed catalog data with factories

--- database/factories/ProductFactory.php
+++ database/factories/ProductFactory.php
@@ -30,4 +30,39 @@ class ProductFactory extends Factory
             'description' => $this->faker->sentence(),
         ];
     }
+
+    public function active(): static
+    {
+        return $this->state(fn (array $attributes) => [
+            'is_active' => true,
+        ]);
+    }
+
+    public function inactive(): static
+    {
+        return $this->state(fn (array $attributes) => [
+            'is_active' => false,
+        ]);
+    }
+
+    public function lowStock(): static
+    {
+        return $this->state(fn (array $attributes) => [
+            'quantity' => $this->faker->numberBetween(0, 5),
+        ]);
+    }
+
+    public function pricedBetween(float $min, float $max): static
+    {
+        return $this->state(fn (array $attributes) => [
+            'price' => $this->faker->randomFloat(2, $min, $max),
+        ]);
+    }
+
+    public function inCurrency(string $currency): static
+    {
+        return $this->state(fn (array $attributes) => [
+            'currency' => strtoupper($currency),
+        ]);
+    }
 }
--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -13,7 +13,9 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        // User::factory(10)->create();
+        $this->call([
+            CategoryProductSeeder::class,
+        ]);
 
         User::factory()->create([
             'name' => 'Test User',
